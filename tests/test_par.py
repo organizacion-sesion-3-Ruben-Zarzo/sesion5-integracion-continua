67d685cc1b436c01b0a3e12f17568a1e0e70c676
Rubén Zarzo González <124666330+RuZaGo@users.noreply.github.com>
2024-02-24 16:52:47 +0100

    Create test_par.py
    Test

---
+++ tests/test_par.py
@@ -0,0 +1,15 @@
+# Librería pytest para ejecutar tests
+import pytest
+
+# Importar archivo de ejercicio
+from ejercicios.operaciones import isPar
+
+# Clase para crear tests. Las funciones de testeo deberán crearse en esta clase
+class TestClass:
+
+    # Test para la operación suma
+    def test_isPar(self):
+        assert isPar(1) == false
+        assert isPar(2) == true
+        assert isPar(23) == false
+        assert isPar(42) == true
